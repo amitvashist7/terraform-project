08d45788ab5d3c876efe4785527c418474f041c6
Amit Vashist <amitvashist7@gmail.com>
2020-02-11 15:51:24 +0530

    First_Deployment has been updates

--- 02-First_Deployment/first_deployment.tf
+++ 02-First_Deployment/first_deployment.tf
@@ -4,6 +4,6 @@ provider "aws" {
   region     = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-XXXXXXXX"
   instance_type = "t2.micro"
 }
