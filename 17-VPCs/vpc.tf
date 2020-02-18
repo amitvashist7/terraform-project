a8eb53000277d1bff1be285fc692f33de8e0b2a8
root <root@openstack.us-central1-a.c.sapient-office-218912.internal>
2020-02-18 16:25:02 +0000

    VPC

--- 17-VPCs/vpc.tf
+++ 17-VPCs/vpc.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
+  version = "1.72.0"
   name = "complete-example"
 
   cidr = "10.10.0.0/16"
 
-  azs             = ["eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1b", "us-east-1c"]
   public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
   private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
 
